28e071614fd12bf0721392e5c8c16e321d5efcf6
Maksim Pavlov <pavlovmark100@gmail.com>
2020-10-04 21:36:13 +0400

    fix: add process exit after error

--- stream.js
+++ stream.js
@@ -6,15 +6,15 @@ const inputStream = require('./inputStream');
 const transformStream = require('./transformStream');
 const outputStream = require('./outputStream');
 module.exports = async (action, shift, inputFilePath, outputFilePath) => {
-    console.log('Проверка:  ', action, shift, inputFilePath, outputFilePath)
-
     if (!action || !shift || !inputFilePath || !outputFilePath) {
         console.error('Enter all data!');
         process.exit(1);
     } else if (!fs.existsSync(inputFilePath)) {
         console.error('Input file doesnt exist!');
+        process.exit(1);
     } else if (!fs.existsSync(outputFilePath)) {
         console.error('Output file doesnt exist!');
+        process.exit(1);
     }
 
 
